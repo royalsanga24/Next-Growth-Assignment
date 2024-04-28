380f6c2edd88bb325473aac88c419918a8e8b753
royalsanga24 <royalsanga24@gmail.com>
2024-04-28 15:11:37 +0530

    Initial login, signup Api setup

--- website/main/main/urls.py
+++ website/main/main/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from .views import login, signup, test_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include("backend.urls")),
+    re_path('login', login),
+    re_path('signup', signup),
+    re_path('test_token', test_token),
 ]
