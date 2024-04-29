921dd5c939fc800c991c0fd7d2ce67b1336e157f
royalsanga24 <royalsanga24@gmail.com>
2024-04-29 18:54:10 +0530

    Functionality to add image added

--- website/main/backend/models.py
+++ website/main/backend/models.py
@@ -16,11 +16,31 @@ class App(models.Model):
     ('education','Education'),
     ('other','Other'),
     )
-    name = models.CharField(max_length=100, blank=False)
+    name = models.CharField(max_length=100, blank=False, unique=True)
     link = models.CharField(max_length=100, blank=False)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, blank=False)
     sub_category = models.CharField(max_length=20, choices=SUB_CATEGORY_CHOICES, blank=False)
     icon = models.ImageField(upload_to=upload_to, blank=True, null=True, validators=[FileExtensionValidator(['png','jpg','gif','jpeg'])])
+
+    def __str__(self):
+        return self.name
+    
+class AppModel(models.Model):
+    CATEGORY_CHOICES = (
+    ('entertaiment','Entertainment'),
+    )
+    SUB_CATEGORY_CHOICES = (
+    ('social media','Social Media'),
+    ('games', 'Games'),
+    ('finance','Finance'),
+    ('education','Education'),
+    ('other','Other'),
+    )
+    name = models.CharField(max_length=100, blank=False)
+    link = models.CharField(max_length=100, blank=False)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, blank=False)
+    sub_category = models.CharField(max_length=20, choices=SUB_CATEGORY_CHOICES, blank=False)
+    
 
     def __str__(self):
         return self.name
--- website/main/backend/views.py
+++ website/main/backend/views.py
@@ -1,6 +1,10 @@
 from main.serializers import AppSerializer
 from .models import App
 
+from io import BytesIO
+import urllib
+import os
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
@@ -9,8 +13,11 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import parser_classes
 from rest_framework.parsers import MultiPartParser, FormParser
 
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.shortcuts import render
 from django.shortcuts import get_list_or_404
+
+from PIL import Image as PILImage
 
 
 
@@ -18,14 +25,24 @@ from django.shortcuts import get_list_or_404
 def app(request):
     apps = get_list_or_404(App)
     serializer = AppSerializer(apps, many=True)
+    print(serializer.data[0]['name'])
     return Response(serializer.data)
 
 @api_view(['POST'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
 @parser_classes([MultiPartParser, FormParser])
 def appCreate(request):
     serializer = AppSerializer(data=request.data, many=False)
-    
     if serializer.is_valid():
         serializer.save()
-
+    else:
+        return Response("Invalid")
+    obj = App.objects.get(name=serializer.data['name'])
+    obj.icon = request.FILES.get('icon')
+    obj.save()
+    print(obj)
+    # serializer.data[0]['icon'] = obj.icon
+    print(dir(serializer.data))
     return Response(serializer.data)
+
